6a537118e43d9353f0527cfbe9dfc380d74d5242
JRgerv <jack.gervais@gmail.com>
2017-02-21 08:42:14 -0800

    finishing touches

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ var time = 30;
 var ammo = 15;
 var score = 0;
 var interval;
+var startBird = 25;
+var showBird = 0;
 
+var birdOrder= [0,4,2,1,3];
 
 $('#start').click(function(){
   // $('#hideBox').fadeTo("slow", 0,function(){});
@@ -10,23 +13,35 @@ $('#start').click(function(){
    interval = setInterval(tick, 1000);
    // $('#ammoCount').append('<img src="/img/ammo.png">');
   addAmmo();
+
 });
 
 
 function tick(){
-  if(time>= 1){
-    time -= 1;
-  $('#timer').text(time);
+  if(time-- > 0){
+  $('#timer').text("0:"+time);
+  if(time%4 == 0){
+    $('.bird').each(function(index){
+      if(birdOrder[showBird] == index) $(this).css('opacity','1');
+      else $(this).css('opacity','0');
+    });
+    showBird++;
+  }
 }else{
     gameOver();
   }
 };
 
 
+  $('#reset').click(function(){
+    location.reload();
+  });
+
+
 // Hide target on click
 function hitTarget(){
   $('img').click(function(){
-    score+=1;
+    score++;
     $(this).fadeTo(2, 0);
     $(this).off();  
     console.log(ammo);
@@ -48,7 +63,7 @@ missTarget();
 
 function addAmmo(){
   for (var i = 0; i < ammo; i++) {
-    $('#ammoCount').append('<img class="count'+i+'" src="../carnival-shooter/img/ammo.png">');
+    $('#ammoCount').append('<img src="../carnival-shooter/img/ammo.png">');
   }
 }
 
@@ -73,3 +88,4 @@ function gameOver(){
     $('#winBox').css('display','block').css('z-index','10');
 }
 
+// 
